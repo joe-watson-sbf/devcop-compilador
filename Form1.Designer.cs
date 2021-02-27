44f239e6b8ec2f62be54cb6ba71f7df4dd06109d
Joe Watson SBF <watsonjoe40@gmail.com>
2021-02-27 18:23:43 -0500

    Muchas revisan para ver si falta algo, en la logica y el diseño tambien...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,16 +30,17 @@ namespace devcop
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.inputArchivo = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnCarga = new System.Windows.Forms.Button();
             this.inputConsola = new System.Windows.Forms.TextBox();
+            this.btnCarga = new System.Windows.Forms.Button();
+            this.inputArchivo = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -59,15 +60,19 @@ namespace devcop
             this.panel1.Size = new System.Drawing.Size(845, 57);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 44);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "DEVCOP";
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.button1.Location = new System.Drawing.Point(764, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
@@ -83,19 +88,15 @@ namespace devcop
             this.label2.Text = "- Compilador";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button1
+            // label1
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(81)))), ((int)(((byte)(133)))));
-            this.button1.Location = new System.Drawing.Point(764, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 57);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "X";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DEVCOP";
             // 
             // panel2
             // 
@@ -106,21 +107,40 @@ namespace devcop
             this.panel2.Controls.Add(this.radioButton1);
             this.panel2.Location = new System.Drawing.Point(39, 91);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(766, 404);
+            this.panel2.Size = new System.Drawing.Size(766, 387);
             this.panel2.TabIndex = 1;
             // 
-            // radioButton1
+            // inputConsola
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(19, 18);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 24);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Archivo";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.inputConsola.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputConsola.Location = new System.Drawing.Point(110, 58);
+            this.inputConsola.Multiline = true;
+            this.inputConsola.Name = "inputConsola";
+            this.inputConsola.Size = new System.Drawing.Size(540, 269);
+            this.inputConsola.TabIndex = 4;
+            // 
+            // btnCarga
+            // 
+            this.btnCarga.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(79)))), ((int)(((byte)(107)))));
+            this.btnCarga.FlatAppearance.BorderSize = 0;
+            this.btnCarga.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCarga.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarga.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnCarga.Location = new System.Drawing.Point(656, 10);
+            this.btnCarga.Name = "btnCarga";
+            this.btnCarga.Size = new System.Drawing.Size(87, 37);
+            this.btnCarga.TabIndex = 3;
+            this.btnCarga.Text = "Cargar";
+            this.btnCarga.UseVisualStyleBackColor = false;
+            this.btnCarga.Click += new System.EventHandler(this.btnCarga_Click);
+            // 
+            // inputArchivo
+            // 
+            this.inputArchivo.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputArchivo.Location = new System.Drawing.Point(110, 18);
+            this.inputArchivo.Name = "inputArchivo";
+            this.inputArchivo.Size = new System.Drawing.Size(540, 25);
+            this.inputArchivo.TabIndex = 2;
             // 
             // radioButton2
             // 
@@ -135,52 +155,51 @@ namespace devcop
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
-            // inputArchivo
+            // radioButton1
             // 
-            this.inputArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inputArchivo.Location = new System.Drawing.Point(110, 18);
-            this.inputArchivo.Name = "inputArchivo";
-            this.inputArchivo.Size = new System.Drawing.Size(540, 29);
-            this.inputArchivo.TabIndex = 2;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(19, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Archivo";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.listBox1);
-            this.panel3.Location = new System.Drawing.Point(39, 531);
+            this.panel3.Location = new System.Drawing.Point(142, 484);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(766, 313);
+            this.panel3.Size = new System.Drawing.Size(547, 283);
             this.panel3.TabIndex = 2;
             // 
-            // btnCarga
+            // label3
             // 
-            this.btnCarga.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(79)))), ((int)(((byte)(107)))));
-            this.btnCarga.FlatAppearance.BorderSize = 0;
-            this.btnCarga.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCarga.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCarga.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.btnCarga.Location = new System.Drawing.Point(656, 16);
-            this.btnCarga.Name = "btnCarga";
-            this.btnCarga.Size = new System.Drawing.Size(97, 31);
-            this.btnCarga.TabIndex = 3;
-            this.btnCarga.Text = "Cargar";
-            this.btnCarga.UseVisualStyleBackColor = false;
-            this.btnCarga.Click += new System.EventHandler(this.btnCarga_Click);
-            // 
-            // inputConsola
-            // 
-            this.inputConsola.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inputConsola.Location = new System.Drawing.Point(112, 68);
-            this.inputConsola.Multiline = true;
-            this.inputConsola.Name = "inputConsola";
-            this.inputConsola.Size = new System.Drawing.Size(538, 269);
-            this.inputConsola.TabIndex = 4;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(79)))), ((int)(((byte)(107)))));
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(4);
+            this.label3.Size = new System.Drawing.Size(155, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Consola de salida";
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(19, 12);
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(3, 39);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(724, 277);
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(537, 238);
             this.listBox1.TabIndex = 0;
             // 
             // Form1
@@ -188,7 +207,7 @@ namespace devcop
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.ClientSize = new System.Drawing.Size(845, 846);
+            this.ClientSize = new System.Drawing.Size(845, 778);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -201,6 +220,7 @@ namespace devcop
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -219,6 +239,7 @@ namespace devcop
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.TextBox inputConsola;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,80 +13,150 @@ namespace devcop
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        string referenciaArchivo, lineaActual;
+
         public Form1()
         {
             InitializeComponent();
             inputConsola.Hide();
             inputArchivo.Show();
-            btnCarga.Location = new Point(656, 18);
             listBox1.Hide();
+
+            this.Width = 845;
+            this.Height = 486;
         }
 
+        // Boton para cerrar la app
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        // La opción "CARGAR POR CONSOLA "
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            if (radioButton2.Checked)
+
+            if (radioButton2.Checked)// SI EL USUARIO ELIGE ESA OPCIÓN, ENTONCES CAMBIAMOS LA GRAFICA
             {
                 inputConsola.Show();
                 inputArchivo.Hide();
-                btnCarga.Location = new Point(336, 351);
-                
+                inputArchivo.ResetText();
 
+                this.Width = 845;
+                this.Height = 486;
+                btnCarga.Location = new Point(336, 333);
+                panel3.Location = new Point(39, 498);
             }
+            
         }
+
+        // La opción "CARGAR POR ARCHIVO "
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            if (radioButton1.Checked)
+            if(radioButton1.Checked) // SI EL USUARIO ELIGE ESA OPCIÓN, ENTONCES CAMBIAMOS LA GRAFICA
             {
-                btnCarga.Location = new Point(656, 18);
+                btnCarga.Location = new Point(656, 10);
+                this.Width = 845;
+                this.Height = 486;
                 inputArchivo.Show();
                 inputConsola.Hide();
+                this.Location = new Point(537, 277);
             }
+            
         }
 
+
+        // button CARGAR
+        /*
+         * Ese buton va hacer 2 trabajos diferentes
+         * 1° - Cuando el usuario montar un archivo, entonces ejecuta el metodo "openfile"
+         * 2° - Si el usuario ingresa datos por consola, ejecuta el metodo "readconsole" 
+         */
         private void btnCarga_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = "c:\\";
-            openFileDialog.RestoreDirectory = true;
+            if (radioButton1.Checked)
+            {
+                onpenfile();
+                inputArchivo.Text = referenciaArchivo;
+                if (inputArchivo.Text.Length > 0)
+                {
+                    panel3.Location = new Point(145, 165);
+                    listBox1.Show();
+                }
+            }
+            else if(radioButton2.Checked)
+            {
 
-            listBox1.Show();
+                readconsole();
+
+                this.Width = 845; this.Height = 778;
+                listBox1.Show();
+                this.Location = new Point(537, 177);
+                panel3.Location = new Point(142, 484);
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona una opción", "DEV COP",MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+
+            
         }
 
 
+
+       
+        /*
+         * METODO PARA ABRIR EL ARCHIVO
+         */
         private void onpenfile()
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-
-
-
-
-
-            // Create a temporary file, and put some data into it.
-            string path = Path.GetTempFileName();
-            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Write, FileShare.None))
+            try
             {
-                Byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
-                // Add some information to the file.
-                fs.Write(info, 0, info.Length);
-            }
-
-            // Open the stream and read it back.
-            using (FileStream fs = File.Open(path, FileMode.Open))
-            {
-                byte[] b = new byte[1024];
-                UTF8Encoding temp = new UTF8Encoding(true);
-
-                while (fs.Read(b, 0, b.Length) > 0)
+                openFileDialog.Filter = "Text Files(.txt)| *.txt"; // mostrarme solo los archivos .txt
+                listBox1.Items.Clear();
+                // OpenFileDialog.ShowDialog() nos ayuda a abrir una ventana para buscar el archivo
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    Console.WriteLine(temp.GetString(b));
+                    referenciaArchivo = openFileDialog.FileName; // obtener la referencia del archivo
+                    StreamReader streamReader = new StreamReader(referenciaArchivo);  // leer el archivo
+                    int contador = 1; // un contador que me permite a imprimir el numero de cada linea de texto del archivo
+                    while (true)
+                    {
+                        lineaActual = streamReader.ReadLine(); // retorna la linea actual
+                        listBox1.Items.Add(contador + "->" + lineaActual); // imprimir en el formato requerido : Numero de la linea con los datos
+
+                        if (streamReader.EndOfStream) // si termina de leer el archivo completamente, parame el Stream y dale un break al Bucle While
+                        {
+                            streamReader.Close();
+                            break;
+                        }
+                        contador++;
+                    }
+
                 }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Se ocurre un error...\nIntente de nuevo.", "DEV COP", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        /*
+        * METODO PARA ABRIR EL ARCHIVO
+        */
+        private void readconsole()
+        {
+            listBox1.Items.Clear();
+            string[] data = inputConsola.Text.Split('\n');
+            int contador = 1;
+            foreach( string lineaActual in data)
+            {
+                listBox1.Items.Add(contador + "->" + lineaActual); // imprimir en el formato requerido : Numero de la linea con los datos
+                contador++;
             }
         }
     }
